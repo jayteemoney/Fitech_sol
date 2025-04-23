aa8fb1b3637a49cfbcba665867ef9141fc25658f
jayteemoney <jethroirmiya@gmail.com>
2025-04-23 14:33:19 +0000

    test

--- contracts/FitechTokens.sol
+++ contracts/FitechTokens.sol
@@ -5,9 +5,12 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract FitechToken is ERC20, Ownable {
-    constructor(address initialOwner) ERC20("FitechToken", "FIT") Ownable(initialOwner) {}
+    constructor(address initialOwner) ERC20("FitechToken", "FIT") Ownable(initialOwner) {
+        _mint(initialOwner, 1000000 * 10**18); // Optional initial supply
+    }
 
-    function mint(address to, uint256 amount) external onlyOwner {
+    // Mint function for testing
+    function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 }
--- test/EthStaking.test.js
+++ test/EthStaking.test.js
@@ -18,42 +18,61 @@ describe("EthStaking", function () {
         await ethStaking.waitForDeployment();
 
         // Fund staking contract with FIT tokens
-        await fitechToken.connect(owner).mint(owner.address, ethers.parseUnits("10000", 18));
-        await fitechToken.connect(owner).approve(ethStaking.target, ethers.parseUnits("10000", 18));
-        await ethStaking.connect(owner).fundRewards(ethers.parseUnits("10000", 18));
+        const amount = ethers.parseUnits("10000", 18);
+        await fitechToken.connect(owner).mint(owner.address, amount);
+        await fitechToken.connect(owner).approve(ethStaking.target, amount);
+        await ethStaking.connect(owner).fundRewards(amount);
+
+        // Verify funding
+        const contractBalance = await fitechToken.balanceOf(ethStaking.target);
+        expect(contractBalance).to.equal(amount, "Contract not funded correctly");
     });
 
     it("Should allow staking ETH", async function () {
-        await ethStaking.connect(user1).stake({ value: ethers.parseEther("1") });
+        const stakeAmount = ethers.parseEther("1");
+        await ethStaking.connect(user1).stake({ value: stakeAmount });
         const stake = await ethStaking.stakes(user1.address);
-        expect(stake.amount).to.equal(ethers.parseEther("1"));
+        expect(stake.amount).to.equal(stakeAmount, "Stake amount incorrect");
+        expect(stake.timestamp).to.be.gt(0, "Stake timestamp not set");
     });
 
     it("Should allow unstaking and reward calculation", async function () {
-        await ethStaking.connect(user1).stake({ value: ethers.parseEther("1") });
+        const stakeAmount = ethers.parseEther("1");
+        await ethStaking.connect(user1).stake({ value: stakeAmount });
 
         // Fast-forward time (30 days)
         await ethers.provider.send("evm_increaseTime", [30 * 24 * 60 * 60]);
-        await ethers.provider.send("evm_mine");
+        await ethers.provider.send("evm_mine", []);
 
         await ethStaking.connect(user1).unstake();
         const reward = await ethStaking.rewards(user1.address);
         console.log("Reward calculated:", ethers.formatUnits(reward, 18));
-        expect(reward).to.be.closeTo(ethers.parseUnits("10", 18), ethers.parseUnits("0.001", 18));
+        expect(reward).to.be.closeTo(
+            ethers.parseUnits("10", 18),
+            ethers.parseUnits("0.001", 18),
+            "Reward calculation incorrect"
+        );
     });
 
     it("Should allow claiming rewards", async function () {
-        await ethStaking.connect(user1).stake({ value: ethers.parseEther("1") });
+        const stakeAmount = ethers.parseEther("1");
+        await ethStaking.connect(user1).stake({ value: stakeAmount });
 
-        // Fast-forward time
+        // Fast-forward time (30 days)
         await ethers.provider.send("evm_increaseTime", [30 * 24 * 60 * 60]);
-        await ethers.provider.send("evm_mine");
+        await ethers.provider.send("evm_mine", []);
 
         await ethStaking.connect(user1).unstake();
+        const reward = await ethStaking.rewards(user1.address);
         await ethStaking.connect(user1).claimRewards();
 
         const balance = await fitechToken.balanceOf(user1.address);
         console.log("User1 FIT balance:", ethers.formatUnits(balance, 18));
-        expect(balance).to.be.closeTo(ethers.parseUnits("10", 18), ethers.parseUnits("0.001", 18));
+        expect(balance).to.be.closeTo(
+            ethers.parseUnits("10", 18),
+            ethers.parseUnits("0.001", 18),
+            "Reward claim incorrect"
+        );
+        expect(await ethStaking.rewards(user1.address)).to.equal(0, "Rewards not reset");
     });
 });
